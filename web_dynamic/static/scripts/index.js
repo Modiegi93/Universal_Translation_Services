23f2cd62c15d7d442fffcb39c961e551c17a5ad5
Mathapelo <modiegi.mphahlele93@gmail.com>
2023-06-19 17:53:37 +0000

    commit

--- web_dynamic/static/scripts/index.js
+++ web_dynamic/static/scripts/index.js
@@ -1,4 +1,13 @@
 $(document).ready(function() {
+
+    $.get('http://0.0.0.0:5001/api/v1/status/', function (data, status) {
+	console.log(data);
+	if (data.status === 'OK') {
+		$('DIV#api_status').addClass('available');
+	} else {
+		$('DIV#api_status').removeClass('available');
+	}
+
     // Open sign-up modal when the "Sign Up" button is clicked
     $("#signupBtn").click(function() {
       $("#signupModal").css("display", "block");
