c28e689610057570026ce96ee3657d8997a297c6
Mathapelo <modiegi.mphahlele93@gmail.com>
2023-06-22 18:08:56 +0000

    start

---
+++ web_dynamic/gunicorn_config.py
@@ -0,0 +1,5 @@
+bind = '0.0.0.0:5000'
+workers = 3
+errorlog = '/home/ubuntu/Universal_Translation_Services/web_dynamic/errorlog'
+accesslog = '/home/ubuntu/Universal_Translation_Services/web_dynamic/accesslog'
+chdir = '/home/ubuntu/Universal_Translation_Services/web_dynamic'
